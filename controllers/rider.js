257a862e047d2c9a8a00901910cb60bd6a5a16be
programmerraja <boooathis123@gmail.com>
2021-04-26 15:15:51 +0530

    Bug fixed

--- controllers/rider.js
+++ controllers/rider.js
@@ -242,295 +242,343 @@ async function getBookedUsers(req, res) {
             await getUsers(index + 1)
           }
         }
-      } //used recursion function so only we can use async await //call only if the Users avalible if (length) { await getUsers(0); let unbooked=parseInt(ride.passenger)-parseInt(booked); res.render("bookedUsers", { seats:ride.passenger, booked:booked, unbooked:unbooked, users: users, rider:req.user }); } } } } async function postMyRideForm(req, res) { if (res.locals.is_correct_ride) { let { from, to, type, model, passenger, amount, time, date }=req.body; let views=type==="taxi" ? "myRideFormTaxi" : "myRideFormGoods" ; //converting to lower case from=from.toLowerCase(); to=to.toLowerCase(); let time_array=convertTimeToString(time); time=time_array[0] + ":" + time_array[1] + " " + time_array[2]; let rider=req.user; let rider_id=rider._id; new_ride=new Ride({ rider_id: rider_id, from, to, type, model, passenger, passenger_left: passenger, amount, time, date }); new_ride.save().catch((err)=> {
-      let msg = dbErrorHandler(err);
-      res.render(views, {
-        msg: msg
-      });
-    });
-  if (new_ride) {
-    res.redirect("/rider/get/myrides/");
-    //after sucessfully created check if it has alert
-    type = type.toLowerCase();
-    let alert = await Alert.find({
-      from: from,
-      to: to,
-      date: {
-        "$eq": date
-      },
-      type: type
-    });
-    let length = alert.length;
-    if (length) {
-      async function sentAlert(index) {
-        //if we find alert get user id and get his email
-        let user_id = alert[index].user_id;
-        let user = await User.findOne({
-          _id: user_id
+      }
+      if (length) {
+        await getUsers(0);
+        let unbooked = parseInt(ride.passenger) - parseInt(booked);
+        res.render("bookedUsers", {
+          seats: ride.passenger,
+          booked: booked,
+          unbooked: unbooked,
+          users: users,
+          rider: req.user
         });
-        if (user) {
-          let email = user.email;
-          let name = user.name;
-          let link = req.protocol + "://" + req.get("host") + "/search/ride/id/" + new_ride._id;
-          let alert_link = req.protocol + "://" + req.get("host") + "/user/unset/alert/" + alert[index]._id;
-          //sending mail to user who set alert
-          let msg = await sendAlertMail(email, name, alert[index], link, alert_link);
-          //call recursively until empty
-          index = index + 1;
-          if (length > index) {
-            await sentAlert(index);
+      }
+    }
+  }
+}
+async function postMyRideForm(req, res) {
+    if (res.locals.is_correct_ride) {
+      let {
+        from,
+        to,
+        type,
+        model,
+        passenger,
+        amount,
+        time,
+        date
+      } = req.body;
+      let views = type === "taxi" ? "myRideFormTaxi" : "myRideFormGoods";
+
+      from = from.toLowerCase();
+      to = to.toLowerCase();
+      let time_array = convertTimeToString(time);
+      time = time_array[0] + ":" + time_array[1] + " " + time_array[2];
+      let rider = req.user;
+      let rider_id = rider._id;
+      new_ride = new Ride({
+        rider_id: rider_id,
+        from,
+        to,
+        type,
+        model,
+        passenger,
+        passenger_left: passenger,
+        amount,
+        time,
+        date
+      });
+      new_ride.save().catch((err) => {
+          let msg = dbErrorHandler(err);
+          res.render(views, {
+            msg: msg
+          });
+        });
+        if (new_ride) {
+          res.redirect("/rider/get/myrides/");
+          //after sucessfully created check if it has alert
+          type = type.toLowerCase();
+          let alert = await Alert.find({
+            from: from,
+            to: to,
+            date: {
+              "$eq": date
+            },
+            type: type
+          });
+          let length = alert.length;
+          if (length) {
+            async function sentAlert(index) {
+              //if we find alert get user id and get his email
+              let user_id = alert[index].user_id;
+              let user = await User.findOne({
+                _id: user_id
+              });
+              if (user) {
+                let email = user.email;
+                let name = user.name;
+                let link = req.protocol + "://" + req.get("host") + "/search/ride/id/" + new_ride._id;
+                let alert_link = req.protocol + "://" + req.get("host") + "/user/unset/alert/" + alert[index]._id;
+                //sending mail to user who set alert
+                let msg = await sendAlertMail(email, name, alert[index], link, alert_link);
+                //call recursively until empty
+                index = index + 1;
+                if (length > index) {
+                  await sentAlert(index);
+                }
+              }
+            }
+            //passing first
+            await sentAlert(0);
           }
         }
       }
-      //passing first
-      await sentAlert(0);
-    }
-  }
-} else {
-  res.render(views, {
-    msg: "Please provide all data"
-  });
-}
-
-}
-async function editMyRideForm(req, res) {
-  if (req.params.id) {
-    //used rider id to avoid other rider to edit the ride
-    let rider_id = req.user._id;
-    let id = req.params.id;
-    let ride = await Ride.findOne({
-      rider_id: rider_id,
-      _id: id
-    });
-    if (ride) {
-      //depend on the type render the correspond form
-      let views = ride.type === "taxi" ? "myRideFormTaxi" : "myRideFormGoods";
-      //converting time format so we can set that as value
-      ride.time = convertTimeToTime(ride.time);
-      res.render(views, {
-        rider: req.user,
-        ride: ride
-      });
-      return
-    }
-  }
-  // render the 404 page
-  res.render("error");
-
-}
-
-async function postEditMyRideForm(req, res) {
-  if (res.locals.is_correct_ride && req.params.id) {
-
-    let id = req.params.id;
-    let {
-      from,
-      to,
-      type,
-      model,
-      passenger,
-      amount,
-      time,
-      date
-    } = req.body;
-    //depend on the type render the correspond form
-    let views = type === "taxi" ? "myRideFormTaxi" : "myRideFormGoods";
-    let time_array = convertTimeToString(time);
-    time = time_array[0] + ":" + time_array[1] + " " + time_array[2];
-
-    let rider = req.user;
-    let rider_id = rider._id;
-
-    ride = await Ride.findOneAndUpdate({
-      rider_id: rider_id,
-      _id: id
-    }, {
-      rider_id: rider_id,
-      from,
-      to,
-      type,
-      model,
-      passenger,
-      passenger_left: passenger,
-      amount,
-      time,
-      date
-    });
-
-
-    if (ride) {
-      res.redirect("/rider/get/myrides/");
-    }
-  } else {
-    res.render(views, {
-      msg: "Please provide all data"
-    });
-  }
-
-}
-
-async function removeMyRideForm(req, res) {
-  if (req.body.id) {
-    let ride_id = req.body.id;
-    let rider = req.user;
-    let rider_id = rider._id;
-    let ride = await Ride.deleteOne({
-      _id: ride_id,
-      rider_id: rider_id
-    });
-    let booking = await Booking.deleteMany({
-      ride_id: ride_id
-    });
-    //need to show rider if some thing bad for better use js in client side
-    if (ride && booking) {
-      res.json({
-        "status": "Sucess",
-        msg: "Successfully Removed"
-      });
-    } else {
-      res.json({
-        "status": "Failure",
-        msg: "Sorry Something went wrong!"
-      });
-    }
-    return;
-  }
-  res.render("error");
-
-}
-
-
-async function forgetPassword(req, res) {
-  res.render("forgetPassword", {
-    rider: req.user
-  })
-
-}
-//handling POST /rider/forget/password
-async function postForgetPassword(req, res) {
-  if (req.body.email) {
-    let email = req.body.email;
-    var rider = await Rider.findOne({
-      email: email
-    });
-    if (rider) {
-      let token = generateToken();
-      let link = req.protocol + "://" + req.get("host") + "/rider/reset/password/" + token;
-
-      //we adding 20 mins to current date and converting in to mili sec
-      let password_reset_expires = Date.now() + 20 * 60 * 1000;
-      //updating the rider token
-      let new_rider = await Rider.findOneAndUpdate({
-        _id: rider._id
-      }, {
-        password_reset_token: token,
-        password_reset_expires: password_reset_expires
-      });
-
-      //sending mail to rider
-      let msg = await sendPasswordReset(rider.email, rider.name, link);
-      if (msg) {
-        res.json({
-          status: "Sucess",
-          msg: "Check your mail to reset the password"
+      else {
+        res.render(views, {
+          msg: "Please provide all data"
         });
+      }
+
+    }
+    async function editMyRideForm(req, res) {
+      if (req.params.id) {
+        //used rider id to avoid other rider to edit the ride
+        let rider_id = req.user._id;
+        let id = req.params.id;
+        let ride = await Ride.findOne({
+          rider_id: rider_id,
+          _id: id
+        });
+        if (ride) {
+          //depend on the type render the correspond form
+          let views = ride.type === "taxi" ? "myRideFormTaxi" : "myRideFormGoods";
+          //converting time format so we can set that as value
+          ride.time = convertTimeToTime(ride.time);
+          res.render(views, {
+            rider: req.user,
+            ride: ride
+          });
+          return
+        }
+      }
+      // render the 404 page
+      res.render("error");
+
+    }
+
+    async function postEditMyRideForm(req, res) {
+      if (res.locals.is_correct_ride && req.params.id) {
+
+        let id = req.params.id;
+        let {
+          from,
+          to,
+          type,
+          model,
+          passenger,
+          amount,
+          time,
+          date
+        } = req.body;
+        //depend on the type render the correspond form
+        let views = type === "taxi" ? "myRideFormTaxi" : "myRideFormGoods";
+        let time_array = convertTimeToString(time);
+        time = time_array[0] + ":" + time_array[1] + " " + time_array[2];
+
+        let rider = req.user;
+        let rider_id = rider._id;
+
+        ride = await Ride.findOneAndUpdate({
+          rider_id: rider_id,
+          _id: id
+        }, {
+          rider_id: rider_id,
+          from,
+          to,
+          type,
+          model,
+          passenger,
+          passenger_left: passenger,
+          amount,
+          time,
+          date
+        });
+
+
+        if (ride) {
+          res.redirect("/rider/get/myrides/");
+        }
       } else {
+        res.render(views, {
+          msg: "Please provide all data"
+        });
+      }
+
+    }
+
+    async function removeMyRideForm(req, res) {
+      if (req.body.id) {
+        let ride_id = req.body.id;
+        let rider = req.user;
+        let rider_id = rider._id;
+        let ride = await Ride.deleteOne({
+          _id: ride_id,
+          rider_id: rider_id
+        });
+        let booking = await Booking.deleteMany({
+          ride_id: ride_id
+        });
+        //need to show rider if some thing bad for better use js in client side
+        if (ride && booking) {
+          res.json({
+            "status": "Sucess",
+            msg: "Successfully Removed"
+          });
+        } else {
+          res.json({
+            "status": "Failure",
+            msg: "Sorry Something went wrong!"
+          });
+        }
+        return;
+      }
+      res.render("error");
+
+    }
+
+
+    async function forgetPassword(req, res) {
+      res.render("forgetPassword", {
+        rider: req.user
+      })
+
+    }
+    //handling POST /rider/forget/password
+    async function postForgetPassword(req, res) {
+      if (req.body.email) {
+        let email = req.body.email;
+        var rider = await Rider.findOne({
+          email: email
+        });
+        if (rider) {
+          let token = generateToken();
+          let link = req.protocol + "://" + req.get("host") + "/rider/reset/password/" + token;
+
+          //we adding 20 mins to current date and converting in to mili sec
+          let password_reset_expires = Date.now() + 20 * 60 * 1000;
+          //updating the rider token
+          let new_rider = await Rider.findOneAndUpdate({
+            _id: rider._id
+          }, {
+            password_reset_token: token,
+            password_reset_expires: password_reset_expires
+          });
+
+          //sending mail to rider
+          let msg = await sendPasswordReset(rider.email, rider.name, link);
+          if (msg) {
+            res.json({
+              status: "Sucess",
+              msg: "Check your mail to reset the password"
+            });
+          } else {
+            res.json({
+              status: "Failure",
+              msg: "Sorry Something went wrong. Please try again"
+            });
+          }
+          return
+        }
         res.json({
           status: "Failure",
-          msg: "Sorry Something went wrong. Please try again"
-        });
+          msg: "No rider exit with given gmail"
+        })
+        return
       }
-      return
+      res.render("error");
+
     }
-    res.json({
-      status: "Failure",
-      msg: "No rider exit with given gmail"
-    })
-    return
-  }
-  res.render("error");
 
-}
-
-//handling GET /rider/reset/password
-async function resetPassword(req, res) {
-  res.render("resetPassword", {
-    rider: req.user
-  })
-}
-
-//handling POST /rider/reset/password
-async function postResetPassword(req, res) {
-
-  if (req.params && req.body.password) {
-
-    let password_reset_token = req.params.id;
-    let new_password = req.body.password;
-    //finding the rider
-    var rider = await Rider.findOne({
-      password_reset_token: password_reset_token,
-      password_reset_expires: {
-        $gt: Date.now()
-      }
-    });
-    if (rider) {
-      let hash = bcrypt.hashSync(new_password, 2);
-      let new_rider = await Rider.findOneAndUpdate({
-        _id: rider._id
-      }, {
-        password: hash
-      });
-      res.json({
-        status: "Sucess",
-        msg: "Password Updated"
-      });
-    } else {
-      res.json({
-        status: "Failure",
-        msg: "Link Expires"
-      });
-    }
-    return
-  }
-  res.render("error");
-}
-
-async function emailVerified(req, res) {
-  if (req.params) {
-    let rider_id = req.params.id;
-    var rider = await Rider.findOne({
-      _id: rider_id
-    });
-    if (rider) {
-      rider.is_email_verified = true;
-      new_rider = await rider.save();
-      res.render("emailVerified", {
+    //handling GET /rider/reset/password
+    async function resetPassword(req, res) {
+      res.render("resetPassword", {
         rider: req.user
-      });
-      return
+      })
     }
-    res.render("error");
-  }
-}
 
-module.exports = {
-  get,
-  getProfilePicture,
-  logout,
-  getProfileById,
-  post,
-  getMyRideFormTaxi,
-  getMyRideFormGoods,
-  getMyRideOptions,
-  getBookedUsers,
-  editMyRideForm,
-  postEditMyRideForm,
-  postMyRideForm,
-  removeMyRideForm,
-  getMyRides,
-  forgetPassword,
-  postForgetPassword,
-  resetPassword,
-  postResetPassword,
-  emailVerified
-};
+    //handling POST /rider/reset/password
+    async function postResetPassword(req, res) {
+
+      if (req.params && req.body.password) {
+
+        let password_reset_token = req.params.id;
+        let new_password = req.body.password;
+        //finding the rider
+        var rider = await Rider.findOne({
+          password_reset_token: password_reset_token,
+          password_reset_expires: {
+            $gt: Date.now()
+          }
+        });
+        if (rider) {
+          let hash = bcrypt.hashSync(new_password, 2);
+          let new_rider = await Rider.findOneAndUpdate({
+            _id: rider._id
+          }, {
+            password: hash
+          });
+          res.json({
+            status: "Sucess",
+            msg: "Password Updated"
+          });
+        } else {
+          res.json({
+            status: "Failure",
+            msg: "Link Expires"
+          });
+        }
+        return
+      }
+      res.render("error");
+    }
+
+    async function emailVerified(req, res) {
+      if (req.params) {
+        let rider_id = req.params.id;
+        var rider = await Rider.findOne({
+          _id: rider_id
+        });
+        if (rider) {
+          rider.is_email_verified = true;
+          new_rider = await rider.save();
+          res.render("emailVerified", {
+            rider: req.user
+          });
+          return
+        }
+        res.render("error");
+      }
+    }
+
+    module.exports = {
+      get,
+      getProfilePicture,
+      logout,
+      getProfileById,
+      post,
+      getMyRideFormTaxi,
+      getMyRideFormGoods,
+      getMyRideOptions,
+      getBookedUsers,
+      editMyRideForm,
+      postEditMyRideForm,
+      postMyRideForm,
+      removeMyRideForm,
+      getMyRides,
+      forgetPassword,
+      postForgetPassword,
+      resetPassword,
+      postResetPassword,
+      emailVerified
+    };
