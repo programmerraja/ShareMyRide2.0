73f1c8ee0151b705854dde0a00a400e449839d90
programmerraja <boooathis123@gmail.com>
2021-05-05 17:30:42 +0530

    Bug fixed

--- middleware/sanitizeHTML.js
+++ middleware/sanitizeHTML.js
@@ -3,10 +3,14 @@ const sanitizeHtml = require("sanitize-html");
 function sanitizeHTML(req, res, next) {
   for (let key of Object.keys(req.body)) {
     //not allowing any tag to user use
-    req.body[key] = sanitizeHtml(req.body[key], {
-      allowedTags: [],
-      allowedAttributes: {}
-    });
+    if(!req.body[key]==="goods&services"){
+
+	    
+	    req.body[key] = sanitizeHtml(req.body[key], {
+	      allowedTags: [],
+	      allowedAttributes: {}
+	    });
+	}
   }
   next();
 }
